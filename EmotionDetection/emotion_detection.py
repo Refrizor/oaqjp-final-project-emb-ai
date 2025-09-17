b611cc38862ba0d0c4411edab4d4999fc31825d6
Refrizor <retizao.tv@gmail.com>
2025-09-16 22:27:45 -0400

    Organization, cleanup

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,8 +3,10 @@ import requests
 
 URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+ERROR_DICT = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
 
-def create_request(text_to_analyze: str) -> list | None:
+
+def create_request(text_to_analyze: str) -> list | dict[str, None]:
     """Sends a POST request to Watson's LLM library
 
     :param text_to_analyze: Input text to be analyzed.
@@ -13,12 +15,17 @@ def create_request(text_to_analyze: str) -> list | None:
 
     text_to_analyze = text_to_analyze.lower()
     request = requests.post(URL, json={"raw_document": {"text": text_to_analyze}}, headers=HEADERS)
+    status = request.status_code
+
+    if status == 400:
+        return ERROR_DICT
 
     try:
-        response = request.json().get("emotionPredictions")
+        response = request.json().get("emotionPredictions", [])
         return response
     except Exception as e:
-        print("Error parsing the response:", request.raise_for_status(), e)
+        print("Error parsing:", e)
+        return ERROR_DICT
 
 def emotion_detector(text: str, verbose: bool = False) -> dict | None:
     """Detects emotions using Watson's LLM library thingy
@@ -29,6 +36,12 @@ def emotion_detector(text: str, verbose: bool = False) -> dict | None:
     """
 
     response = create_request(text)
+
+    # If Watson shoots a 400, create_request already returned the dict of Nones
+    if isinstance(response, dict) and response["dominant_emotion"] is None:
+        return response
+
+    # Otherwise, proceed
     emotion_dict = response[0]["emotion"]
 
     dominant_emotion = get_dominant_emotion(emotion_dict)
--- server.py
+++ server.py
@@ -12,6 +12,10 @@ def index():
 def get_emotion_detector():
     text = request.args.get('textToAnalyze')
     emotion_data = emotion_detector(text, True)
+
+    if emotion_data["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
     dominant_emotion = emotion_data.pop('dominant_emotion')
 
     parts = [f"'{k}': {v}" for k, v in emotion_data.items()]
