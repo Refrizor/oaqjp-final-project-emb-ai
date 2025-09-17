5c579e5b15bee6c200f53613b6ed888f97a535f2
Refrizor <retizao.tv@gmail.com>
2025-09-16 21:00:43 -0400

    Fix testing

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -7,17 +7,17 @@ class Test(TestCase):
         self.assertEqual(res["dominant_emotion"], "joy")
 
     def test_anger(self):
-        res = emotion_detector("I am glad this happened", True)
+        res = emotion_detector("I am really mad about this	", True)
         self.assertEqual(res["dominant_emotion"], "anger")
 
     def test_disgust(self):
-        res = emotion_detector("I am glad this happened", True)
+        res = emotion_detector("I feel disgusted just hearing about this", True)
         self.assertEqual(res["dominant_emotion"], "disgust")
 
     def test_sadness(self):
-        res = emotion_detector("I am glad this happened", True)
+        res = emotion_detector("I am so sad about this	", True)
         self.assertEqual(res["dominant_emotion"], "sadness")
 
     def test_fear(self):
-        res = emotion_detector("I am glad this happened", True)
+        res = emotion_detector("I am really afraid that this will happen	", True)
         self.assertEqual(res["dominant_emotion"], "fear")
