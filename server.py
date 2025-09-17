5d79a26bbf4046b1b221e2daa95e81729d541f97
Refrizor <retizao.tv@gmail.com>
2025-09-16 21:50:46 -0400

    Refactor to affirm to project criteria

--- server.py
+++ server.py
@@ -12,8 +12,19 @@ def index():
 def get_emotion_detector():
     text = request.args.get('textToAnalyze')
     emotion_data = emotion_detector(text, True)
-    dominant_emotion = emotion_data['dominant_emotion']
-    output = f"For the given statement, the system response is {emotion_data}. The dominant emotion is: <b>{dominant_emotion}</b>"
+    dominant_emotion = emotion_data.pop('dominant_emotion')
+
+    parts = [f"'{k}': {v}" for k, v in emotion_data.items()]
+
+    if len(parts) > 1:
+        pretty = ", ".join(parts[:-1]) + " and " + parts[-1]
+    else:
+        pretty = parts[0]
+
+    output = (
+        f"For the given statement, the system response is {pretty}. "
+        f"The dominant emotion is <b>{dominant_emotion}</b>."
+    )
     return output
 
 if __name__ == "__main__":
